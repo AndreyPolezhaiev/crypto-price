986195e70d5be119b90249b52b3ab8c890d5ea16
Andrey Polezhaiev <workpolezhaiev@gmail.com>
2024-04-30 23:52:17 +0300

    added bad case of checking symbol

--- src/main/java/com/polezhaiev/cryptoprice/controller/CryptoPriceController.java
+++ src/main/java/com/polezhaiev/cryptoprice/controller/CryptoPriceController.java
@@ -13,10 +13,20 @@ public class CryptoPriceController {
     private static final String BINANCE_API_URL = "https://fapi.binance.com/fapi/v1/premiumIndex";
 
     @GetMapping("/price")
-    public CryptoDataResponseDto getCryptoPrice(@RequestParam(required = false) String symbol) {
+    public CryptoDataResponseDto getCryptoPrice(@RequestParam String symbol) {
         String apiUrl = BINANCE_API_URL;
         if (symbol != null && !symbol.isEmpty()) {
             apiUrl += "?symbol=" + symbol;
+        } else {
+            return new CryptoDataResponseDto()
+                    .setIndexPrice(null)
+                    .setMarkPrice(null)
+                    .setEstimatedSettlePrice(null)
+                    .setTime(null)
+                    .setSymbol(null)
+                    .setLastFundingRate(null)
+                    .setNextFundingTime(null)
+                    .setInterestRate(null);
         }
 
         RestTemplate restTemplate = new RestTemplate();
--- src/main/java/com/polezhaiev/cryptoprice/dto/CryptoDataResponseDto.java
+++ src/main/java/com/polezhaiev/cryptoprice/dto/CryptoDataResponseDto.java
@@ -1,15 +1,17 @@
 package com.polezhaiev.cryptoprice.dto;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class CryptoDataResponseDto {
     private String symbol;
     private String markPrice;
     private String indexPrice;
     private String estimatedSettlePrice;
     private String lastFundingRate;
-    private long nextFundingTime;
+    private Long nextFundingTime;
     private String interestRate;
-    private long time;
+    private Long time;
 }
