9c480e021355f25ab8d89f46f29545a8af72aad6
Andrey Polezhaiev <workpolezhaiev@gmail.com>
2024-04-30 22:42:25 +0300

    add dependencies

---
+++ README.md
@@ -0,0 +1,1 @@
+This app allows to get price of specific cryptocurrency.
---
+++ src/main/java/com/polezhaiev/cryptoprice/CryptoPriceApplication.java
@@ -0,0 +1,13 @@
+package com.polezhaiev.cryptoprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoPriceApplication.class, args);
+	}
+
+}
